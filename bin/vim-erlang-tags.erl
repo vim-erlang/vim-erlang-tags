8b2aa8afa548fa9edd7597bb9b5efde26c79557e
Csaba Hoch <csaba.hoch@erlang-solutions.com>
2013-10-21 11:55:31 +0200

    Document --ignore

--- bin/vim-erlang-tags.erl
+++ bin/vim-erlang-tags.erl
@@ -156,6 +156,10 @@ Options:
   -             Read the list of files from the standard input.
   -o, --output FILE
                 Write the output into the given file instead of ./tags.
+  --i, --ignore FILE_WILDCARD
+                Ignore the files/directories that match the given wildcard.
+                Read http://www.erlang.org/doc/man/filelib.html#wildcard-1 for
+                the wildcard patterns.
 
 Example:
   $ vim-erlang-tags.erl
